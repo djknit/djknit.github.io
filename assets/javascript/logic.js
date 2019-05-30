61d9c984fbad967e56ccc311c071759f379b995e
David <djknit@gmail.com>
2019-05-29 20:16:44 -0500

    bug fix: animation replay

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -38,6 +38,9 @@ $(window).scroll(setSideMenuState);
 
 function setSideMenuState() {
   const sideNavMenu = $("#side-nav-menu");
+  // remove animation so it doesn't replay
+  $("#side-nav-menu > nav").attr("style", null);
+  // mark #side-nav-menu with min screen width to show menu on so that it sits right below about section header
   if (window.pageYOffset < 284) {
     sideNavMenu.attr("state", "hide");
   }
@@ -57,16 +60,17 @@ function setSideMenuState() {
 
 // open/close side nav menu
 $("#side-nav-toggle").on("click", function() {
+  const sideNav = $("#side-nav-menu > nav");
   if ($(this).attr("state") === "open") {
     $(this).attr("state", "closed");
-    $("#side-nav-menu > nav").attr("style", "animation-name: close-side; animation-duration: 1s; -webkit-animation-fill-mode: forwards;");
-    $("#side-nav-menu > nav").attr("state", "closed");
+    sideNav.attr("style", "animation-name: close-side; animation-duration: 1s; -webkit-animation-fill-mode: forwards;");
+    sideNav.attr("state", "closed");
     $("#side-nav-toggle").attr("title", "Open navigation menu");
   }
   else {
     $(this).attr("state", "open");
-    $("#side-nav-menu > nav").attr("style", "animation-name: open-side; animation-duration: 1s; -webkit-animation-fill-mode: forwards;");
-    $("#side-nav-menu > nav").attr("state", "open");
+    sideNav.attr("style", "animation-name: open-side; animation-duration: 1s; -webkit-animation-fill-mode: forwards;");
+    sideNav.attr("state", "open");
     $("#side-nav-toggle").attr("title", "Close navigation menu");
   }
 });
